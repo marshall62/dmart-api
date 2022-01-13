78bc996d8804c76b6233b3d036eb205211f303e6
David Marshall <david.marshall@wwt.com>
2022-01-13 18:43:40 -0500

    resize images with diff quality

--- resize.py
+++ resize.py
@@ -7,30 +7,31 @@ import glob
 
 
 # go over the whole dir and create subdirs for medium and thumb
-def resize_dir (dir, make_thumbs=False, make_midsize=False):
+def resize_dir (dir, make_thumbs="False", make_midsize="False"):
+  make_thumbs = make_thumbs == "True"
+  make_midsize = make_midsize == 'True'
   images = [file for file in os.listdir(dir) if file.endswith(('jpeg', 'JPEG', 'JPG', 'png', 'jpg'))]
   if make_thumbs and not os.path.exists(os.path.join(dir,"thumb")):
     os.mkdir(os.path.join(dir,"thumb"))
   if make_midsize and not os.path.exists(os.path.join(dir, "midsize")):
     os.mkdir(os.path.join(dir, "midsize"))
-  for image in sorted(images):
-      img = Image.open(os.path.join(dir,image))
+  for image in sorted(images):     
       print(f"resizing {image}")
       if make_midsize:
+        img = Image.open(os.path.join(dir,image))
         img = make_thumbnail(img,700)
-        img.save(os.path.join(dir, "midsize", image), optimize=True, quality=100)
+        img.save(os.path.join(dir, "midsize", image), "JPEG")
       if make_thumbs:
+        img = Image.open(os.path.join(dir,image))
         img = make_thumbnail(img,100)
-        img.save(os.path.join(dir,"thumb",image), optimize=True, quality=95)
+        img.save(os.path.join(dir,"thumb",image), "JPEG")
 
-# writes to the same location and just appends _thumb
-def resize_path (path):
+# writes to the subdir
+def resize_path (path, size=100):
     img = Image.open(path)
     path, ext = os.path.splitext(path)
-    print("Saving thumbnail as ", path+"_thumb"+ext)
-    # print(img._getexif().items())
-    img = make_thumbnail(img)
-    img.save(path+"_thumb"+ext)
+    img = make_thumbnail(img, size)
+    img.save(path+f"_resized_{size}"+ext)
 
 def make_thumbnail (img: Image, size=100):
   if img._getexif():
